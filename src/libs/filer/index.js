04b352103fcba064efc9ff470bb0aa609d13143a
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-05-07 08:44:39 +0700

    filter non-md extension file + add npm ignore folder

--- src/libs/filer/index.js
+++ src/libs/filer/index.js
@@ -1,10 +1,12 @@
 import directoryScanner from './directoryScanner'
 import fileMapper from './fileMapper'
 import mdFileReader from './mdFileReader'
+import getExtension from './getExtension'
 
 const filer = async () => {
   const rootDir = process.cwd()
-  const files = await directoryScanner()
+  let files = await directoryScanner()
+  files = files.filter(file => (getExtension(file) === 'md'))
   const map = fileMapper(files)
   let docs = await Promise.all(files.map(path => mdFileReader(`${rootDir}/docs/${path}`)))
   docs = docs.filter(item => item.document)
