4f227b3c72161df6e4141a04fd6424de21cf8e3c
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-05-17 17:39:31 +0700

    add styles for api docs

--- src/genFile.js
+++ src/genFile.js
@@ -10,31 +10,12 @@ const htmlForm = (data, deepLevel) => {
   return `<!DOCTYPE html>
   <html>
   <head>
-    <style>
-    .item1 { grid-area: header; }
-    .item2 { grid-area: menu; }
-    .item3 { grid-area: main; }
-    
-    .grid-container {
-      display: grid;
-      grid-template-areas:
-        'header header header header header header'
-        'menu main main main right right'
-        'menu footer footer footer footer footer';
-      grid-gap: 10px;
-    }
-
-    ul{
-      padding-left: 25px;
-    }
-    </style>
     <link rel="stylesheet" type="text/css" href="./${pathToRoot}styles/index.css">
   </head>
   <body>
   <div class="grid-container">
-    <div class="item1">${title}</div>
-    <div class="item2">${indexString}</div>
-    <div class="item3">${dataInDoc}</div>  
+    <div class="menu">${indexString}</div>
+    <div class="page">${dataInDoc}</div>  
   </div>
   
   </body>
@@ -47,7 +28,7 @@ const genFile = async (data, settings, deepLevel) => {
   const text = htmlForm(data, deepLevel)
   let dir = path.replace('.md', '.html')
   dir = dir.replace(settings.source, `${settings.destination}/${settings.source}`)
-  smartFs.smartWrite(`.${dir}`, [text])
+  await smartFs.smartWrite(`.${dir}`, [text])
 }
 
 export default genFile
