8b48057ab4870ec2928bb3432ea461833f428f7e
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-08-20 12:50:29 +0700

    Update css compiler engine

--- src/copyCSSFolder.js
+++ src/copyCSSFolder.js
@@ -1,12 +1,19 @@
 import fs from 'fs-extra'
+import less from 'less'
+import LessPluginCleanCSS from 'less-plugin-clean-css'
 import { PATH, ERROR_MESSAGE } from './constant'
 import log from './utils/log'
+
+const cleanCSSPlugin = new LessPluginCleanCSS({ advanced: true })
 
 const copyCSSFolder = async (destinationFolder) => {
   const rootDir = process.cwd()
   const dir = __dirname
+  const lessSheet = await fs.readFileSync(`${dir}/${PATH.STYLES}/index.less`)
+  const compiledLessSheet = await less.render(lessSheet.toString(), { plugins: [cleanCSSPlugin] })
   try {
-    await fs.copySync(`${dir}/${PATH.STYLES}`, `${rootDir}/${destinationFolder}/${PATH.STYLES}`)
+    await fs.ensureDirSync(`${rootDir}/${destinationFolder}/${PATH.STYLES}`)
+    await fs.writeFileSync(`${rootDir}/${destinationFolder}/${PATH.STYLES}/index.css`, compiledLessSheet.css)
     log.compile('Generating Themes...')
   } catch (error) {
     log.error(ERROR_MESSAGE.CSS_COPY_ERROR)
